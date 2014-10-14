146a63c92f771c9f3ab006e3e4d3c1c84cd4244e
ido172 <ido172@gmail.com>
2014-10-14 22:58:55 +0300

    action screen partial

---
+++ DiaperClient/public_html/js/scripts.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+$( document ).ready(function() {
+    var selectedAction;
+    
+    function onPageReady() {
+        /*
+          $.ajax({
+    url: "test.html",
+    context: document.body
+  }).done(function() {
+    $( this ).addClass( "done" );
+  })
+  */
+    }
+    
+    
+    function onActionScreenReady() {
+        selectedAction = &
+    }
+
+});
